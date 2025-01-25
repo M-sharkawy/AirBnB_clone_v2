42d5a440740c44f62255e0321dc68fa756041ded
M-sharkawy <mostafasharks021@gmail.com>
2025-01-25 16:28:01 +0200

    do_deploy file edits

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,13 +20,13 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, '/tmp/')
-        sudo(f"mkdir -p {_path}")
-        sudo(f"tar -xzvf {file_name} -C {_path}")
-        sudo(f"rm -f '/tmp/'{file_name}")
-        sudo(f"mv {_path}/web_static/* {_path}")
-        sudo(f"rm -rf {_path}/web_static")
-        sudo(f"rm -rf /data/web_static/current")
-        sudo(f"ln -s {_path} /data/web_static/current")
+        run(f"mkdir -p {_path}")
+        run(f"tar -xzvf {file_name} -C {_path}")
+        run(f"rm -f '/tmp/'{file_name}")
+        run(f"mv {_path}/web_static/* {_path}")
+        run(f"rm -rf {_path}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {_path} /data/web_static/current")
         return True
     except:
         return False
