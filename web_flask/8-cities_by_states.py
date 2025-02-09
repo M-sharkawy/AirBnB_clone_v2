519cee86e4641e35d02cac98e9206dc7d7b6e8c7
M-sharkawy <mostafasharks021@gmail.com>
2025-02-10 00:32:17 +0200

    task 9 edits

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ def states_list():
     Displays an HTML page with a list of all State objects in DBStorage
     """
     states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
