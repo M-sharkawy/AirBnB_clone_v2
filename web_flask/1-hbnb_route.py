02bd8e64450a9b4af439fa160349cab9f38e9f3b
M-sharkawy <mostafasharks021@gmail.com>
2025-02-09 20:17:03 +0200

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,5 +9,10 @@ app = Flask(__name__)
 def root():
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
